349563d4c93713fb3b59bd36d94a86e846d1f9d0
Arif_Nabi <edinlink92@gmail.com>
2025-03-24 16:48:42 +0500

    Some cases are changed

--- question_1.py
+++ question_1.py
@@ -1,21 +1,20 @@
 # Find Majority Element in List
-# Give: [1, 7, 8, 7, 7, 7]
+# Given: [1, 7, 8, 7, 7, 7]
 # Excepted Output: 7
-
-
+from typing import List
 
 numbers = [1, 3,4,5,6,7,2,2,7,7,7]
 
 # 1st solution
-def find_majority_number(numbers):
-    my_dict = {}
+def find_majority_number(numbers: List[int]) -> str:
+    my_dict: dict = {}
     majority_num = None
     for number in numbers:
         if number in my_dict:
             my_dict[number] += 1
         else:
             my_dict[number] = 1
-    max_num = 0
+    max_num: int = 0
     for num in my_dict:
         if my_dict[num] > max_num:
             max_num  = my_dict[num]
@@ -27,10 +26,13 @@ print(result)
 
 
 # 2nd solution
-majority_number = None
+def find_majority_number2(numbers: List[int]) -> str:
+    majority_number = None
 
-for num in numbers:
-    count = numbers.count(num)
-    majority_number = num
+    for num in numbers:
+        count = numbers.count(num)
+        majority_number = num
+    return f"Majority number: {majority_number}, repeted {count} times"
     
-print(f"Majority number: {majority_number}, repeted {count} times")
+result = find_majority_number2(numbers)
+print(result) 
