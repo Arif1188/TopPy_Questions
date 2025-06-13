34c31b9e116c3ae5578082c39d510cb4f797cce1
Arif1188 <edinlink92@gmail.com>
2025-06-14 00:14:19 +0500

    Renamed files

--- Medium/question_5.py
+++ Medium/question_5.py
@@ -1,28 +1,13 @@
-# Write a function to generate a list of palindrome numbers within a given range.
-# Given: 10 to 100
-# Excepted Output: [11, 22, 33, 44, 55, 66, 77, 88, 99]
+# The function takes in the credit card number as an argument.
+# Inside the function replace all the digits of the card number with asterisks ("*"), except for the last four digits.
+# Return the new credit card number.
+# Given: 4444444444444444
+# Excepted Output: ************4444
 
-# 1st solution with for cycle
-def palindrome_numbers(start: int,end: int) ->list:
-    my_list: list = []
-    for i in range(start,end):
-        num_str: str = str(i)
-        if num_str == num_str[::-1]:
-            my_list.append(int(num_str))
-    return my_list
-
-palindrome = palindrome_numbers(10,100)
-print(palindrome)
-        
-# 2nd solution with while cycle      
-def palindrome_numbers2(start: int, end: int) -> list:
-    my_list: list = []
-    while start <= end:
-        num_str: str = str(start)
-        if num_str == num_str[::-1]:
-            my_list.append(num_str)
-        start +=1
-    return my_list
-
-palindrome2 = palindrome_numbers(100,200)
-print(palindrome2)
+def replacde_digits(card_number: int) -> str:
+    num_str: str = str(card_number)
+    return "*" * (len(num_str)-4) + num_str[-4:]
+    
+            
+result = replacde_digits(4444444444444444)
+print(result)
