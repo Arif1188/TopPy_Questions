83ce021909cb74af4aae659ba7ebe3b08389c5f6
Arif1188 <edinlink92@gmail.com>
2025-10-21 23:52:11 +0500

    Added some comment for ex.py

--- Easy/ex.py
+++ Easy/ex.py
@@ -7,6 +7,18 @@
 # Open brackets must be closed in the correct order.
 # Every close bracket has a corresponding open bracket of the same type.
 
+# Example 1:
+# Input: s = "()"
+# Output: true
+
+# Example 2:
+# Input: s = "()[]{}"
+# Output: true
+
+# Example 3:
+# Input: s = "(]"
+# Output: false
+
 class Solution(object):
     def isValid(self, s):
         """
